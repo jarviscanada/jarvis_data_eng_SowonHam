450be1f4296ba8d90b436026be6e3eabe805eb36
Sowon Ham <hammsowon@gmail.com>
2021-12-30 00:13:21 +0000

    Finish host_info and host_usage bash scripts

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -3,7 +3,12 @@
 # Usage of the script
 # ./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
 
+# Pseudocode for `host_usage` bash script
 # assign CLI arguments to variables (e.g. `psql_host=$1`)
+# parse host hardware specifications using bash cmds
+# Construct the INSERT statement
+# Execute the INSERT statement through psql CLI tool
+
 psql_host=$1
 psql_port=$2
 db_name=$3
@@ -16,9 +21,9 @@ if [ $# -ne 5 ]; then
   exit 1
 fi
 
-# parse host hardware specifications using bash cmds
 specs=`lscpu`
 hostname=$(hostname -f) # column 1 in host_info table
+
 
 cpu_number=$(echo "$specs" | grep ^CPU\(s\): | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$specs" | grep Architecture | awk '{print $2}' | xargs)
@@ -28,8 +33,26 @@ l2_cache=$(echo "$specs" | grep L2 | awk '{print $3}' | xargs)
 total_mem=$(echo "$(cat /proc/meminfo)" | grep MemTotal | awk '{print $2}' | xargs)
 timestamp=$(echo "$(vmstat -t)" | awk '{print $18,$19}' | tail -n 1 | xargs)
 
-# construct the INSERT statement
+# Get the host ID by checking the current `host_info` table
+# 1. If empty then it is going to be ID 1
+# 2. Check the last ID # in the table and the new info will be (last ID# + 1)
+last_host_id="(SELECT id
+FROM host_info
+ORDER BY id DESC
+LIMIT 1);";
+
+host_id=$(($last_host_id + 1))
+
+# INSERT the server usage data into the PSQL host_usage table
+insert_stmt="INSERT INTO host_info
+VALUES('$host_id', '$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache',
+'$total_mem', '$timestamp');";
 
 
+# Set up the environmental variable PSQL password
+export PGPASSWORD=$psql_password
 
-# execute the INSERT statement through psql CLI tool
+# Insert the data into the database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+exit $?
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -3,7 +3,12 @@
 # Usage of the script
 # ./scripts/host_info.sh psql_host psql_port db_name psql_user psql_password
 
+# Pseudocode for `host_usage` bash script
 # assign CLI arguments to variables (e.g. `psql_host=$1`)
+# parse host hardware specifications using bash cmds
+# Construct the INSERT statement
+# Execute the INSERT statement through psql CLI tool
+
 psql_host=$1
 psql_port=$2
 db_name=$3
@@ -16,7 +21,6 @@ if [ $# -ne 5 ]; then
   exit 1
 fi
 
-# parse host hardware specifications using bash cmds
 vmstat_mb=$(vmstat --unit M) # in units of MB
 hostname=$(hostname -f) # hostname used to subquery a matching id
 
@@ -28,10 +32,19 @@ cpu_kernel=$(echo "$vmstat_mb" | awk '{print $14}' | tail -n 1 | xargs)
 disk_io=$(echo "$(vmstat -d)" | awk '{print $10}' | tail -n 1 | xargs)
 disk_available=$(echo "$(df -BM)" | grep /dev/sda2 | awk '{print $4}' | xargs)
 
-# construct the INSERT statement
 # Subquery to find matching id in host_info table
 host_id="(SELECT id
 FROM host_info
-WHERE hostname='$hostname')";
+WHERE hostname='$hostname');";
 
-# execute the INSERT statement through psql CLI tool
+# INSERT the server usage data into the PSQL host_usage table
+insert_stmt="INSERT INTO host_usage
+VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', $disk_available');";
+
+# Set up the environmental variable PSQL password
+export PGPASSWORD=$psql_password
+
+# Insert the data into the database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+
+exit $?
